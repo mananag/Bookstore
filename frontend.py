fa3931fd3ea8edad790d3e71ae6239c2905e2f6a
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2021-01-03 01:09:08 +0530

    Executive file created

--- frontend.py
+++ frontend.py
@@ -114,7 +114,7 @@ b4.grid(row=5, column=3)
 b5 = Button(window, text="Delete", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6 = Button(window, text="Close", width=12, command=window.destroy())
+b6 = Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 window.title("Bookstore")
